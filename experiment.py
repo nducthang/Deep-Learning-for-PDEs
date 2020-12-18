97482b548fbc55556c06932a4643d8812aeb66a7
nducthang <nguyenducthang8a2@gmail.com>
2020-12-18 13:44:54 +0700

    add

--- experiment.py
+++ experiment.py
@@ -8,21 +8,21 @@ from Model.HL import PDENetLight
 from Model.NeuralNetwork import NeuralNetwork
 
 
-PATH_MODEL = './results/test8/'
-PATH_LOSS_IMAGE = './results/test8/loss.png'
-PATH_L2_ERROR_IMAGE = './results/test8/l2_error.png'
-PATH_LOSS_TXT = './results/test8/loss.txt'
-PATH_L2_ERROR_TXT = './results/test8/l2_error.txt'
+PATH_MODEL = './results/test12/'
+PATH_LOSS_IMAGE = './results/test12/loss.png'
+PATH_L2_ERROR_IMAGE = './results/test12/l2_error.png'
+PATH_LOSS_TXT = './results/test12/loss.txt'
+PATH_L2_ERROR_TXT = './results/test12/l2_error.txt'
 
-NUM_ITERATOR = 1000
+NUM_ITERATOR = 2000
 
 if __name__ == '__main__':
     # model = PDENetLight(3, 10)
     # dim_input, dim_output, hidden_size, num_hidden_layer
-    model = NeuralNetwork(3, 1, 16, 1)
+    model = NeuralNetwork(2, 1, 16, 2)
     model.init_weights()
 
-    problem = Problem_3(model)
+    problem = Problem_1(model)
     problem.train(num_iterator=NUM_ITERATOR, path_model=PATH_MODEL)
     problem.plot_loss(PATH_LOSS_IMAGE)
     problem.plot_l2_error(PATH_L2_ERROR_IMAGE)
--- visualize.py
+++ visualize.py
@@ -5,25 +5,26 @@ import numpy as np
 import math
 import random
 
-model_path = './weights/problem_laplace_1.bin'
+model_path = './results/test7/model_2000.bin'
 model = torch.load(model_path)
 
 
 def u(x, y): return 3+y+0.5*(y**2-x**2)
-
+def u2(x, y): return np.sin(np.pi*x)*np.sin(np.pi*y)
 
 def u_predict(x, y):
     data = [x, y]
     data_tensor = torch.Tensor(data).resize(2, 1)
     return model(data_tensor).item()
 
-def visualize(num_element=1000):
-    a1, a2, b1, b2 = -math.sqrt(6), math.sqrt(6), -math.sqrt(6), math.sqrt(6)
+def visualize(num_element=100):
+    # a1, a2, b1, b2 = -math.sqrt(6), math.sqrt(6), -math.sqrt(6), math.sqrt(6)
+    a1, a2, b1, b2 = 0, 1, 0, 1
     x1 = np.linspace(a1, a2, num=num_element)
     x2 = np.linspace(b1, b2, num=num_element)
     X, Y = np.meshgrid(x1, x2)
 
-    Z = u(X, Y)
+    Z = u2(X, Y)
 
     fig = plt.figure()
     ax = plt.axes(projection="3d")
@@ -37,14 +38,14 @@ def visualize(num_element=1000):
         for j in range(num_element):
             Z_predict[i][j] = u_predict(X[i][j], Y[i][j])
 
-    ax.plot_surface(X, Y, Z, color='red') # truth
-    ax.plot_surface(X, Y, Z_predict, color='blue') # predict
-    plt.show(block=False)
-    plt.draw()
-    plt.pause(10)
-    # ax.plot_wireframe(X, Y, Z, color='green')
-    # ax.plot_wireframe(X, Y, Z_predict, color='blue')
-    # plt.show()
+    # ax.plot_surface(X, Y, Z, color='red') # truth
+    # ax.plot_surface(X, Y, Z_predict, color='blue') # predict
+    # plt.show(block=False)
+    # plt.draw()
+    # plt.pause(10)
+    ax.plot_wireframe(X, Y, Z, color='green')
+    ax.plot_wireframe(X, Y, Z_predict, color='blue')
+    plt.show()
 
 if __name__ == '__main__':
     visualize()
